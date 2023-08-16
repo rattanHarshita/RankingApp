1fa80990ea76a218e58e754c217a4429a06685dd
Harshy <Harshy@DESKTOP-TUQCAE1>
2023-08-16 23:50:12 +1000

    Add project files.

---
+++ RankingApp/Models/ItemModel.cs
@@ -0,0 +1,12 @@
+﻿namespace RankingApp.Models
+{
+    public class ItemModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int ImageId { get; set; }
+        public int Ranking { get; set; }
+        //Movie-1; Album - 2
+        public int ItemType { get; set; }
+    }
+}
